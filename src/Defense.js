dadb6e8f5d434945a8679dec24b0998467936f25
seburoh <seburoh@outlook.com>
2023-05-05 10:56:03 -0700

    js stuff

--- src/Defense.js
+++ src/Defense.js
@@ -1,48 +1,115 @@
+const scn = require('prompt-sync')({sigint: true});
 //regex for matching a name string
 const nameRegex = RegExp("^(?!-)(?!.*-{2})[-a-zA-Z]{1,50}(?<!-)$");
 
+var ErrorLog = {
+    logData : "",
+    append : function(str) {
+        this.logData += str + '\n';
+    }
+};
+
+var InputData = {
+    firstName : "",
+    lastName : "",
+    firstInt : "",
+    secondInt : "",
+    hashedPasswordPath : "",
+    inputFilePath : "",
+    inputFileData : "",
+    outputFilePath : "",
+    currentTime : "",
+    storedSalt : "",
+    addNumbers : function() {
+        return firstInt + secondInt;
+    },
+    multiplyNumbers : function() {
+        return firstInt * secondInt;
+    },
+    toString : function() {
+        return "First Name: " + firstName + '\n' +
+        "Last Name: " + lastName + '\n' +
+        "Added Numbers: " + addNumbers() + '\n' +
+        "Multiplied Numbers: " + multiplyNumbers() + '\n' +
+        "Input File Contents:\n" + inputFileData + '\n';
+    }
+};
+
 console.log("Starting program!");
 nameInputPhase();
-console.log("Moving to Integer phase!");
+numberInputPhase();
+
+function numberInputPhase () {
+    console.log("Enter an integer between values -2147483648 and 2147483647.");
+    let num = scn("Enter a number: ");
+    //if the user inputs a false name, prompt user for name again
+    while (!numberValidator(num)) {
+        printFailure("Value not accepted as integer: " + num);
+        num = scn("Please try again: ");
+    }
+    InputData.firstInt = num;
+    printSuccess("Integer " + InputData.firstInt + " has been accepted!");
+
+    console.log("Enter an integer between values -2147483648 and 2147483647.");
+    num = scn("Enter another number: ");
+    //if the user inputs a false name, prompt user for name again
+    while (!numberValidator(num)) {
+        printFailure("Value not accepted as integer: " + num);
+        num = scn("Please try again: ");
+    }
+    InputData.secondInt = num;
+    printSuccess("Integer " + InputData.secondInt + " has been accepted!");
+}
+
+function numberValidator (num) {
+    return Number.isInteger(parseInt(num)) && num < 2147483648 && num > -2147483649; 
+}
 
 //function that goes through the first and last name inputs
 function nameInputPhase () {
+    ErrorLog.append("Reached first name phase");
     console.log("Please enter a name");
-    console.log("Max 50 characters, alphabetic characters, and dashes only. Dashes cannot be chained.");
+    console.log("Max 50 characters in first name and last name each.");
+    console.log("Alphabetic characters, and dashes only. Dashes cannot be chained.");
     console.log("Name cannot begin or end with a dash.");
     console.log("Apologies that if you are Elon Musk's kid, you cannot use this service.");
-
     //had to install prompt modules using npm install prompt-sync
     //accept user input for first name
-    let firstName = prompt("Enter a first name:");
-
+    let firstName = scn("Enter a first name: ");
     //if the user inputs a false name, prompt user for name again
-    while (!nameValidator(firstName) | firstName == null) {
-        firstName = prompt("Please enter another first name");
+    while (!nameValidator(firstName)) {
+        printFailure("Regex did not match for first name: " + firstName);
+        firstName = scn("Please enter another first name: ");
     }
-
-    //if user input matches a name continue on in the program
-    console.log("First name" + firstName + "has been accepted!");
+    InputData.firstName = firstName;
+    printSuccess("First name " + firstName + " has been accepted!");
 
     //accept user input for last name
-    let lastName = prompt("Enter a last name:");
-
-    //if the user inputs a false name, prompt user for name again
-    while (!nameValidator(firstName) | lastName == null) {
-        lastName = prompt("Please enter another last name");
+    ErrorLog.append("Reached last name phase");
+    console.log("Max 50 characters in first name and last name each.");
+    console.log("Alphabetic characters, and dashes only. Dashes cannot be chained.");
+    console.log("Name cannot begin or end with a dash.");
+    console.log("Apologies that if you are Elon Musk's kid, you cannot use this service.");
+    let lastName = scn("Enter a last name: ");
+    while (!nameValidator(lastName)) {
+        printFailure("Regex did not match for last name: " + lastName);
+        lastName = scn("Please enter another last name: ");
     }
-
-    //if user input matches a name continue on in the program
-    console.log("First name" + lastName + "has been accepted!");
+    InputData.lastName = lastName;
+    printSuccess("Last name " + lastName + " has been accepted!");
 }
 
 //validates a string as a name using nameRegex
 function nameValidator(name) {
-    if (!nameRegex.exec(name)) {
-        console.log();("Invalid Name inputted!");
-        return false;
-    } else {
-        return true;
-    }
+    return nameRegex.exec(name);
 }
 
+function printSuccess(message) {
+    console.log("SUCCESS: " + message);
+    ErrorLog.append("SUCCESS: " + message);
+}
+
+function printFailure(message) {
+    console.log("ERROR: " + message);
+    ErrorLog.append("ERROR: " + message);
+}
--- src/Scratch.java
+++ src/Scratch.java
@@ -3,25 +3,8 @@ import java.util.Scanner;
 
 public class Scratch {
     public static void main(String[] args) {
-        while (true) {
-            Scanner scn = new Scanner(System.in);
-            System.out.println(testInt(scn.nextLine()));
-            String inp = String.valueOf(scn.nextLine());
-            long l = Long.parseLong(inp);
-            int i = Integer.parseInt(inp);
-            System.out.println(l);
-            System.out.println(i);
-            System.out.println((int)l == i);
-        }
+        String regex = "^[+-]?[0-9a-f]{1,8}$";
+        System.out.println("0a".matches(regex));
     }
 
-    private static boolean testInt(final String input) {
-        try {
-            Integer.parseInt(input);
-        } catch (Exception e) {
-            System.out.println(Arrays.toString(e.getStackTrace()));
-            return false;
-        }
-        return true;
-    }
 }
